e92289b3448c219140a68254d3614af11cf7adff
Harry Park <harry@harrypark.io>
2017-06-12 21:33:25 +1000

    basic dispatching

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // electron-process-comms
 
-import electron from 'electron'
+// import electron from 'electron';
 
 // determines process originating from
 const Process = {
@@ -24,13 +24,78 @@ const Process = {
 	},
 	is: (type) => {
 		if (typeof type === 'string') {
-			return type === this.type()
+			return type === this.type();
 		} else {
-			throw new TypeError('type of `type` was not string')
+			throw new TypeError('type of `type` was not string');
 		}
 	}
 }
 
-export default {
-
+const isPromise = (val) => {
+	return val && typeof val.then === 'function';
 }
+
+class ProcessComms {
+	constructor(options = {}) {
+		let { actions = {} } = options;
+
+		this._actions = Object.create(null);
+
+		const store = this
+
+		Object.keys(actions).forEach((action) => {
+			registerAction(store, action, actions[action], store)
+		})
+
+		const { dispatch, commit } = this;
+
+		this.dispatch = function boundDispatch (type, payload) {
+			return dispatch.call(store, type, payload);
+		}
+	}
+
+	dispatch(_type, _payload) {
+		const { type, payload } = {
+			type: _type,
+			payload: _payload
+		};
+
+		const entry = this._actions[type];
+
+		if (!entry) {
+			console.error(`unknown action type: ${type}`);
+			return;
+		}
+
+		return entry.length > 1 ? Promise.all(entry.map(handler => handler(payload))) : entry[0](payload);
+	}
+}
+
+const registerAction = (store, type, handler, local) => {
+	const entry = store._actions[type] || (store._actions[type] = []);
+	entry.push((payload, cb) => {
+		let res = handler({
+			dispatch: local.dispatch
+		}, payload, cb);
+
+		if (!isPromise(res)) {
+			res = Promise.resolve(res);
+		}
+
+		return res
+	})
+}
+
+const processComms = new ProcessComms({
+	actions: {
+		init: ({ dispatch }) => {
+			console.log('hello')
+			dispatch('init2')
+		},
+		init2: ({ dispatch }) => {
+			console.log('bye')
+		}
+	}
+});
+
+processComms.dispatch('init')
